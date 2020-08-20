c5e715e2e6f2d61b1a9c5be5370ab278f1bb2037
selfshore <1343274273@qq.com>
2020-08-20 19:41:02 +0800

    更新

--- 企业公示系统/run.py
+++ 企业公示系统/run.py
@@ -18,10 +18,11 @@ r = requests.get('http://www.gsxt.gov.cn/index.html', headers=headers)
 cookie = dict_from_cookiejar(r.cookies)
 js = 'function jsl(){window = {navigator: {userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Geck' \
      'o) Chrome/83.0.4103.116 Safari/537.36"},outerWidth: 1920,outerHeight: 1050,};location = {reload:function () {' \
-     '}};document = {};' + re.findall('<script>(.*?)</script>', r.text)[0] + ';return document.cookie}'
+     '}};document = {};setTimeout = function (x) {x()};' + re.findall('<script>(.*?)</script>', r.text)[0] + ';return document.cookie}'
 ctx = execjs.compile(js)
 jsl_cookie = ctx.call('jsl')
 jsl_cookie = re.findall('=(.*?);', jsl_cookie)[0]
 cookie.update({"__jsl_clearance": jsl_cookie})
 r = requests.get('http://www.gsxt.gov.cn/index.html', headers=headers, cookies=cookie)
 print(r.text)
+
